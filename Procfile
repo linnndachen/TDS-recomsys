5756aaf737baa31e7fe1228bf8bf91f2856b6b2e
wchen928 <lchen.msc2019@ivey.ca>
2020-11-30 21:33:41 -0800

    update:port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:APP
+web: gunicorn --bind 0.0.0.0:$PORT app:APP
--- app.py
+++ app.py
@@ -96,4 +96,4 @@ APP = create_app()
 
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=3001, debug=True)
+    APP.run(host='0.0.0.0', port=3000, debug=True)
