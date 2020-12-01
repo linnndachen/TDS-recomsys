7a15bf8d15dd328484283e092b800ec0d4768a1d
wchen928 <lchen.msc2019@ivey.ca>
2020-11-30 19:50:18 -0800

    add:create_app

--- app.py
+++ app.py
@@ -15,108 +15,111 @@ from sqlalchemy import exc, create_engine
 import sqlite3
 import joblib
 
-app = Flask(__name__)
-# create and configure the app
-app.secret_key = 'SECRET'
-CORS(app, resources={"/": {"origins": "*"}})
+def create_app(test_config=None)
+    app = Flask(__name__)
+    # create and configure the app
+    app.secret_key = 'SECRET'
+    CORS(app, resources={"/": {"origins": "*"}})
 
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers',
-                         'Content-Type, Authorization, true')
-    response.headers.add('Access-Control-Allow-Methods',
-                         'GET, PATCH, PUT, POST,DELETE, OPTIONS')
-    return response
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                            'Content-Type, Authorization, true')
+        response.headers.add('Access-Control-Allow-Methods',
+                            'GET, PATCH, PUT, POST,DELETE, OPTIONS')
+        return response
 
 
-def input_vec(input, model):
-    avgword2vec = None
-    count = 0
-    for word in input.split():
-        if word in model:
-            count += 1
-            if avgword2vec is None:
-                avgword2vec = model[word]
-            else:
-                avgword2vec = avgword2vec + model[word]
+    def input_vec(input, model):
+        avgword2vec = None
+        count = 0
+        for word in input.split():
+            if word in model:
+                count += 1
+                if avgword2vec is None:
+                    avgword2vec = model[word]
+                else:
+                    avgword2vec = avgword2vec + model[word]
 
-    if avgword2vec is not None:
-        avgword2vec = avgword2vec / count
+        if avgword2vec is not None:
+            avgword2vec = avgword2vec / count
 
-    return avgword2vec
-
-
-def clean_text(s):
-    # removeNonAscii
-    s = "".join(i for i in s if ord(i) < 128)
-
-    # return all lower cases
-    s = s.lower()
-
-    # remove stop wrods
-    s = s.split()
-    stops = set(stopwords.words("english"))
-    text = [w for w in s if not w in stops]
-    text = " ".join(text)
-
-    # remove html
-    html_pattern = re.compile('<.*?>')
-    text = html_pattern.sub(r'', text)
-
-    # remove punctuation
-    text = re.sub(r'[^\w\s]', " ", text)
-    return text
+        return avgword2vec
 
 
-def recomm_engine(string, vec_df, model):
-    # vectorize the input
-    cleaned_input = clean_text(string)
-    input_vector = input_vec(cleaned_input, model)
+    def clean_text(s):
+        # removeNonAscii
+        s = "".join(i for i in s if ord(i) < 128)
 
-    # finding cosine similarity for the vectors
-    similarity = []
-    input_vector = input_vector.reshape(1, -1)
-    for n in vec_df['vectors']:
-        n = n.reshape(1, -1)
-        scores = cosine_similarity(input_vector, n)[0][0]
-        similarity.append(scores)
+        # return all lower cases
+        s = s.lower()
 
-    vec_df['similarity'] = similarity
+        # remove stop wrods
+        s = s.split()
+        stops = set(stopwords.words("english"))
+        text = [w for w in s if not w in stops]
+        text = " ".join(text)
 
-    # sort and find the recommended movie
-    vec_df = vec_df.sort_values(by=['similarity'], ascending=False)
+        # remove html
+        html_pattern = re.compile('<.*?>')
+        text = html_pattern.sub(r'', text)
 
-    return vec_df.loc[::, :'Title']
-
-
-vec_df = pd.read_pickle("models/vectors.pkl")
-model = joblib.load("models/glove_model.pkl")
+        # remove punctuation
+        text = re.sub(r'[^\w\s]', " ", text)
+        return text
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    show_df = vec_df.loc[::, :'Title']
-    return render_template('/index.html',  tables=[show_df.to_html()], titles=show_df.columns.values)
+    def recomm_engine(string, vec_df, model):
+        # vectorize the input
+        cleaned_input = clean_text(string)
+        input_vector = input_vec(cleaned_input, model)
+
+        # finding cosine similarity for the vectors
+        similarity = []
+        input_vector = input_vector.reshape(1, -1)
+        for n in vec_df['vectors']:
+            n = n.reshape(1, -1)
+            scores = cosine_similarity(input_vector, n)[0][0]
+            similarity.append(scores)
+
+        vec_df['similarity'] = similarity
+
+        # sort and find the recommended movie
+        vec_df = vec_df.sort_values(by=['similarity'], ascending=False)
+
+        return vec_df.loc[::, :'Title']
 
 
-# web page that handles user query and displays model results
-@app.route('/result')
-def go():
-    # save user input in query
-    query = request.args.get('query', '')
-
-    # use model to predict classification for query
-    recomm_orders = recomm_engine(query, vec_df, model)
-
-    # This will render the go.html Please see that file.
-    return render_template('/index.html',  tables=[recomm_orders.to_html()], titles=recomm_orders.columns.values)
+    vec_df = pd.read_pickle("models/vectors.pkl")
+    model = joblib.load("models/glove_model.pkl")
 
 
-def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        show_df = vec_df.loc[::, :'Title']
+        return render_template('/index.html',  tables=[show_df.to_html()], titles=show_df.columns.values)
+
+
+    # web page that handles user query and displays model results
+    @app.route('/result')
+    def go():
+        # save user input in query
+        query = request.args.get('query', '')
+
+        # use model to predict classification for query
+        recomm_orders = recomm_engine(query, vec_df, model)
+
+        # This will render the go.html Please see that file.
+        return render_template('/index.html',  tables=[recomm_orders.to_html()], titles=recomm_orders.columns.values)
+
+
+    return app
+
+
+APP = create_app()
 
 
 if __name__ == '__main__':
-    main()
+    APP.run(host='0.0.0.0', port=3001, debug=True)
